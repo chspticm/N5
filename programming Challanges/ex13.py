9d74c1944fe084e48a84feb1c7f5a810a89d59da
chspticm <nlstrattons@northlan.org.uk>
2023-05-25 14:38:51 +0100

    added 13

---
+++ programming Challanges/ex13.py
@@ -0,0 +1,9 @@
+# Mr Stratton
+# 25/05/2023
+# Programming Challenges 13
+
+print('Would you like some advice?')
+answer = input('>')
+
+if answer == 'Y':
+    print('Always know where your towel is.')
