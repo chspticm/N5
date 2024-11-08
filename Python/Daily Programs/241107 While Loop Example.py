5fdf51291557675e0e1311b6decc9b555e1bb50a
chspticm <nlstrattons@northlan.org.uk>
2024-11-08 11:16:11 +0000

    241108 Weekly Update

---
+++ Python/Daily Programs/241107 While Loop Example.py
@@ -0,0 +1,8 @@
+total = 0
+
+while total < 5:
+    total = total + 1
+    print(total)
+    
+    
+print('Final result of total =',total)
---
+++ Python/Daily Programs/Conditional Loop.py
@@ -0,0 +1,11 @@
+# Conditional Loops
+# 4/11/24
+
+total = 0
+while total < 10:
+    print('How much did the item cost')
+    item = float(input('>'))
+    
+    total = total + item
+
+print('the total was',total)
